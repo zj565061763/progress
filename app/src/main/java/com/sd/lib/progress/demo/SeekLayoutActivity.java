1ffa4a6ffe59bd67b8e2fa16d74336bbd9094788
zhengjun <565061763@qq.com>
2019-06-17 21:40:21 +0800

    demo

---
+++ app/src/main/java/com/sd/lib/progress/demo/SeekLayoutActivity.java
@@ -0,0 +1,35 @@
+package com.sd.lib.progress.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.sd.lib.progress.seek.FSeekLayout;
+import com.sd.lib.progress.seek.SeekLayout;
+
+public class SeekLayoutActivity extends AppCompatActivity
+{
+    public static final String TAG = SeekLayoutActivity.class.getSimpleName();
+
+    private FSeekLayout mSeekLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seek_layout);
+        mSeekLayout = findViewById(R.id.seek_layout);
+
+        mSeekLayout.setOrientation(SeekLayout.Orientation.Horizontal);
+        mSeekLayout.setMaxProgress(100);
+        mSeekLayout.setMinProgress(20);
+        mSeekLayout.setOnProgressChangeCallback(new SeekLayout.OnProgressChangeCallback()
+        {
+            @Override
+            public void onProgressChanged(SeekLayout seekLayout, int progress, boolean isTouch)
+            {
+                Log.i(TAG, "onProgressChanged: " + progress + " isTouch:" + isTouch);
+            }
+        });
+    }
+}
