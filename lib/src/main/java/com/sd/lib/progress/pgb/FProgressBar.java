710ccc3179276d35d2715362af9436c4fdff5e81
zhengjun <565061763@qq.com>
2019-06-26 20:59:01 +0800

    内部改为ImageView渲染

--- lib/src/main/java/com/sd/lib/progress/pgb/FProgressBar.java
+++ lib/src/main/java/com/sd/lib/progress/pgb/FProgressBar.java
@@ -1,21 +1,13 @@
 package com.sd.lib.progress.pgb;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.sd.lib.progress.ProgressHolder;
 import com.sd.lib.progress.ProgressView;
@@ -30,15 +22,11 @@ public class FProgressBar extends View implements ProgressView
 
     private Orientation mOrientation = Orientation.Horizontal;
 
-    private final Paint mPaint = new Paint();
-    private final Rect mProgressBitmapRect = new Rect();
-    private final RectF mProgressRectF = new RectF();
-
-    private Bitmap mProgressBitmap;
-
+    private ImageView mImageView;
     private int mProgressImage;
     private int mProgressColor;
 
+    private final Rect mProgressRect = new Rect();
     private boolean mReverseProgress = false;
 
     private ProgressHolder mHolder;
@@ -46,6 +34,8 @@ public class FProgressBar extends View implements ProgressView
     public FProgressBar(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        mImageView = new ImageView(context);
+        mImageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
         if (getBackground() == null)
             setBackgroundColor(Color.parseColor("#999999"));
@@ -176,8 +166,9 @@ public class FProgressBar extends View implements ProgressView
         {
             mProgressImage = resId;
 
-            mProgressBitmap = null;
             mProgressColor = 0;
+            mImageView.setBackgroundColor(0);
+            mImageView.setImageResource(resId);
 
             invalidate();
         }
@@ -194,8 +185,9 @@ public class FProgressBar extends View implements ProgressView
         {
             mProgressColor = color;
 
-            mProgressBitmap = null;
             mProgressImage = 0;
+            mImageView.setBackgroundColor(color);
+            mImageView.setImageBitmap(null);
 
             invalidate();
         }
@@ -211,143 +203,59 @@ public class FProgressBar extends View implements ProgressView
         mReverseProgress = reverse;
     }
 
-    private Bitmap getProgressBitmap()
-    {
-        final int viewWidth = getWidth();
-        final int viewHeight = getHeight();
-
-        if (viewWidth == 0 || viewHeight == 0)
-            return null;
-
-        if (mProgressImage == 0 && mProgressColor == 0)
-            return null;
-
-        if (mProgressBitmap == null)
-        {
-            if (mProgressImage != 0)
-            {
-                mProgressBitmap = imageResIdToBitmap(getContext(), mProgressImage);
-                if (mProgressBitmap == null)
-                    mProgressBitmap = imageResIdToBitmap(getContext(), mProgressImage, viewWidth, viewHeight);
-            } else if (mProgressColor != 0)
-            {
-                mProgressBitmap = colorToBitmap(mProgressColor, viewWidth, viewHeight);
-            }
-        }
-
-        return mProgressBitmap;
-    }
-
-    private RectF getProgressRectF()
+    private Rect getProgressRect()
     {
         if (mOrientation == Orientation.Horizontal)
         {
-            mProgressRectF.top = 0;
-            mProgressRectF.bottom = getHeight();
+            mProgressRect.top = 0;
+            mProgressRect.bottom = getHeight();
 
             if (mReverseProgress)
             {
-                mProgressRectF.left = getWidth() * (1 - getProgressPercent());
-                mProgressRectF.right = getWidth();
+                mProgressRect.left = (int) (getWidth() * (1 - getProgressPercent()));
+                mProgressRect.right = getWidth();
             } else
             {
-                mProgressRectF.left = 0;
-                mProgressRectF.right = getWidth() * getProgressPercent();
+                mProgressRect.left = 0;
+                mProgressRect.right = (int) (getWidth() * getProgressPercent());
             }
         } else
         {
-            mProgressRectF.left = 0;
-            mProgressRectF.right = getWidth();
+            mProgressRect.left = 0;
+            mProgressRect.right = getWidth();
 
             if (mReverseProgress)
             {
-                mProgressRectF.top = 0;
-                mProgressRectF.bottom = getHeight() * getProgressPercent();
+                mProgressRect.top = 0;
+                mProgressRect.bottom = (int) (getHeight() * getProgressPercent());
             } else
             {
-                mProgressRectF.top = getHeight() * (1 - getProgressPercent());
-                mProgressRectF.bottom = getHeight();
+                mProgressRect.top = (int) (getHeight() * (1 - getProgressPercent()));
+                mProgressRect.bottom = getHeight();
             }
         }
-        return mProgressRectF;
+        return mProgressRect;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mImageView.measure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom)
+    {
+        super.onLayout(changed, left, top, right, bottom);
+        mImageView.layout(left, top, right, bottom);
     }
 
     @Override
     protected void onDraw(Canvas canvas)
     {
         super.onDraw(canvas);
-
-        final Bitmap bitmap = getProgressBitmap();
-        if (bitmap != null)
-        {
-            final RectF progressRectF = getProgressRectF();
-
-            mProgressBitmapRect.left = (int) progressRectF.left;
-            mProgressBitmapRect.top = (int) progressRectF.top;
-            mProgressBitmapRect.right = (int) progressRectF.right;
-            mProgressBitmapRect.bottom = (int) progressRectF.bottom;
-
-            scaleBitmapIfNeed();
-            canvas.drawBitmap(bitmap, mProgressBitmapRect, progressRectF, mPaint);
-        }
-    }
-
-    private void scaleBitmapIfNeed()
-    {
-        final int viewWidth = getWidth();
-        final int viewHeight = getHeight();
-
-        if (viewWidth == 0 || viewHeight == 0)
-            return;
-
-        if (mProgressBitmap.getWidth() != viewWidth
-                || mProgressBitmap.getHeight() != viewHeight)
-        {
-            mProgressBitmap = Bitmap.createScaledBitmap(mProgressBitmap, viewWidth, viewHeight, true);
-        }
-    }
-
-    private static Bitmap imageResIdToBitmap(Context context, int resId)
-    {
-        if (resId == 0)
-            return null;
-
-        final Resources resources = context.getResources();
-        final TypedValue typedValue = new TypedValue();
-        resources.openRawResource(resId, typedValue);
-
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inDensity = typedValue.density;
-
-        return BitmapFactory.decodeResource(resources, resId, options);
-    }
-
-    private static Bitmap imageResIdToBitmap(Context context, int resId, int width, int height)
-    {
-        if (resId == 0)
-            return null;
-
-        final Drawable drawable = context.getResources().getDrawable(resId);
-        return drawableToBitmap(drawable, width, height);
-    }
-
-    private static Bitmap colorToBitmap(int color, int width, int height)
-    {
-        if (color == 0)
-            return null;
-
-        final Drawable drawable = new ColorDrawable(color);
-        return drawableToBitmap(drawable, width, height);
-    }
-
-    private static Bitmap drawableToBitmap(Drawable drawable, int width, int height)
-    {
-        final Bitmap bitmap = Bitmap.createBitmap(width, height,
-                drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
-
-        final Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, width, height);
-        drawable.draw(canvas);
-        return bitmap;
+        canvas.clipRect(getProgressRect());
+        mImageView.draw(canvas);
     }
 }
